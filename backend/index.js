d3dca9e5b2bc5755b4c760c183ff70342513b2eb
Namasvi <you@example.com>
2025-09-08 12:23:47 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,9 @@ app.use("/upload", uploadRoutes);
 app.use("/filter", filterRoutes);
 app.use("/calculate", calculateRoutes);
 app.use("/download", downloadRoutes);
+app.get("/", (req, res) => {
+  res.send("🚀 API is running... Use /upload, /filter, /calculate, /download");
+});
 
 app.listen(PORT, () =>
   console.log(` Server running on http://localhost:${PORT}`)
